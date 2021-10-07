c5ed8fbf7aa942310c089ef7990d7702f0995d62
dreamjh20 <dreamjh20@gmail.com>
2021-10-07 20:26:20 +0900

    CREATE: urls using include

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from pymoon import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pymoon/', views.index),
+    path('pymoon/', include('pymoon.urls')),
 ]
