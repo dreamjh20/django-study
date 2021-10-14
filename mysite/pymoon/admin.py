c5f8497ae5880a2839a13ec4169ef2a0689a2082
dreamjh20 <dreamjh20@gmail.com>
2021-10-14 19:58:04 +0900

    FEAT: Search Question

--- mysite/pymoon/admin.py
+++ mysite/pymoon/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Question
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+admin.site.register(Question, QuestionAdmin)
