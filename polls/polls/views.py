58d93c5d25e1adca832869c08cca67ed89dca377
dreamjh20 <dreamjh20@gmail.com>
2021-12-28 21:43:31 +0900

    ADD: Set View Page

--- polls/polls/views.py
+++ polls/polls/views.py
@@ -4,12 +4,13 @@ from django.shortcuts import render
 from .models import Question
 
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
